f5f0b191259596d5ab35ef1ae647945ea5348cb6
Georg Hess <georg.hess@zenseact.com>
2022-10-13 08:39:45 +0200

    great naming

--- scripts/yolov7.sh
+++ scripts/yolov7.sh
@@ -13,4 +13,4 @@ singularity exec --bind /datasets:/datasets --bind /staging:/staging --bind /wor
     --bind /workspaces/s0001387/yolov7/coco:/coco/ \
     --pwd /workspaces/s0001387/yolov7 \
     /staging/agp/geohe/containers/yolov7.sif \
-    python -u train.py --workers 8 --device 0 --batch-size 32 --data data/custom.yaml --img 640 640 --cfg cfg/training/yolov7-custom.yaml --weights 'yolov7_training.pt' --name yolov7-custom --hyp data/hyp.scratch.custom.yaml
+    python -u train.py --workers 8 --device 0 --batch-size 32 --data data/custom.yaml --img 640 640 --cfg cfg/training/yolov7_single_cls.yaml --weights 'yolov7_training.pt' --name yolov7-custom --hyp data/hyp.scratch.custom.yaml
