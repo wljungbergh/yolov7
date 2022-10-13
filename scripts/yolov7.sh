fe8da7ce627ae366e575dcdb6c0d0e2a64ca4705
Georg Hess <georg.hess@zenseact.com>
2022-10-13 08:47:55 +0200

    naming is hard

--- scripts/yolov7.sh
+++ scripts/yolov7.sh
@@ -3,8 +3,6 @@
 #SBATCH --nodes 1
 #SBATCH --ntasks-per-node 1
 #SBATCH --gres=gpu:1
-#SBATCH --cpus-per-task 16
-#SBATCH --mem-per-gpu 64G
 #SBATCH --output /workspaces/%u/pmb-nll/logs/slurm-%j-run.out
 #SBATCH --partition zprod
 
@@ -13,4 +11,4 @@ singularity exec --bind /datasets:/datasets --bind /staging:/staging --bind /wor
     --bind /workspaces/s0001387/yolov7/coco:/coco/ \
     --pwd /workspaces/s0001387/yolov7 \
     /staging/agp/geohe/containers/yolov7.sif \
-    python -u train.py --workers 8 --device 0 --batch-size 32 --data data/custom.yaml --img 640 640 --cfg cfg/training/yolov7_single_cls.yaml --weights 'yolov7_training.pt' --name yolov7-custom --hyp data/hyp.scratch.custom.yaml
+    python -u train.py --workers 8 --device 0 --batch-size 32 --data data/densepose.yaml --img 640 640 --cfg cfg/training/yolov7_single_cls.yaml --weights 'yolov7_training.pt' --name yolov7-custom --hyp data/hyp.scratch.custom.yaml
