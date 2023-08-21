3776476b2d8708b946743e9dc44dd4cd6b16c85d
William Ljungbergh <william.ljungbergh@zenseact.com>
2023-08-21 10:08:43 +0200

    Add batching

--- cache_features.py
+++ cache_features.py
@@ -7,30 +7,17 @@ import os
 from models.yolo_encoder import Encoder
 from utils.datasets import create_dataloader
 from utils.general import (
-    coco80_to_coco91_class,
     check_dataset,
     check_file,
     check_img_size,
-    check_requirements,
-    box_iou,
-    non_max_suppression,
-    scale_coords,
-    xyxy2xywh,
-    xywh2xyxy,
-    set_logging,
-    increment_path,
     colorstr,
 )
 from utils.torch_utils import select_device
 
-def main(
-    data,
-    batch_size=32,
-    imgsz=640,
-    half_precision=True
-):
+
+def main(data, batch_size=32, imgsz=640, half_precision=True):
     device = select_device(opt.device, batch_size=batch_size)
-    
+
     enc = Encoder(opt.cfg).to(device)
 
     gs = max(int(enc.stride.max()), 32)  # grid size (max stride)
@@ -52,13 +39,10 @@ def main(
     # Dataloader
     if device.type != "cpu":
         enc(
-            torch.zeros(1, 3, imgsz, imgsz)
-            .to(device)
-            .type_as(next(enc.parameters()))
+            torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(enc.parameters()))
         )  # run once
     task = "test"
-    
-    assert batch_size == 1, "Feature caching assumes batch size 1"
+
     dataloader = create_dataloader(
         data[task],
         imgsz,
@@ -70,21 +54,24 @@ def main(
         prefix=colorstr(f"{task}: "),
     )[0]
 
-    for batch_i, (img, targets, paths, shapes) in enumerate(tqdm(dataloader)):
+    for img, _, paths, _ in tqdm(dataloader, desc=f"Caching features for {task}"):
         img = img.to(device, non_blocking=True)
         img = img.half() if half else img.float()  # uint8 to fp16/32
         img /= 255.0  # 0 - 255 to 0.0 - 1.0
         with torch.no_grad():
             # Run model
             out = enc(img, augment=False)  # inference and training outputs
-            torch.save(out.squeeze(0), f"{opt.out_folder}/{task}/{os.path.basename(paths[0]).strip('.jpg')}.pt")
+
+        for i, path in enumerate(paths):
+            torch.save(
+                out[i],
+                f"{opt.out_folder}/{task}/{os.path.basename(path).strip('.jpg')}.pt",
+            )
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="cache_features.py")
-    parser.add_argument(
-        "--cfg", type=str, default=None, help="config path"
-    )
+    parser.add_argument("--cfg", type=str, default=None, help="config path")
     parser.add_argument(
         "--data", type=str, default="data/coco.yaml", help="*.data path"
     )
@@ -101,7 +88,10 @@ if __name__ == "__main__":
         "--single-cls", action="store_true", help="treat as single-class dataset"
     )
     parser.add_argument(
-        "--out-folder", type=str, help="path to the directory where cached features should be stored", required=True
+        "--out-folder",
+        type=str,
+        help="path to the directory where cached features should be stored",
+        required=True,
     )
     opt = parser.parse_args()
     opt.data = check_file(opt.data)  # check file
